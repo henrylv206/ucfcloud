a1533e8888487e7f1d834dda3dd9ff131f6ae4c5
HenryLv <henrylv206@qq.com>
2014-07-01 17:44:08 +0800

    use github.com to save code

---
+++ ucfgroup-openstack/src/main/java/org/ucfgroup/ucfcloud/openstack/App.java
@@ -0,0 +1,13 @@
+package org.ucfgroup.ucfcloud.openstack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
